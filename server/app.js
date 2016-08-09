8e69370935860a0d5d1dbc79f116c1e0df6bd385
Kris Szafranski <kdszafranski@gmail.com>
2016-08-09 15:17:13 -0500

    static files done

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 app.set('port', process.env.PORT || 5000);
+
+// catch-all for static files
+app.get('/*', function(req, res) {
+  var file = req.params[0] || '/views/index.html';
+  console.log('file', file);
+  res.sendFile(path.join(__dirname, './public', file));
+});
 
 app.listen(app.get('port'), function() {
   console.log('server is running on port ', app.get('port'));
