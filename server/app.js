bf936d98898977576299d8e2d252afc9d39cc266
Kris Szafranski <kdszafranski@gmail.com>
2016-08-09 14:40:15 -0500

    added in base folders and files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
-// Node app
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function() {
+  console.log('server is running on port ', app.get('port'));
+});
