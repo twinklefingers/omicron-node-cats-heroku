b44bc60a7aa52452e256a2e275c1121ed4b9e1ed
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-08-11 15:28:08 -0500

    Challenge Complete

--- server/app.js
+++ server/app.js
@@ -2,31 +2,23 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var cats = require('./routes/cats');
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-// data array
-var cats = [{name: 'Luna', trait: 'Fiesty'}];
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Routes
-app.get('/cat', function(req, res) {
-  res.send(cats);
-});
-
-app.post('/new', function(req, res) {
-  console.log('request: ', req.body);
-  cats.push(req.body);
-  res.sendStatus(201);
-});
+app.use('/cats', cats);
 
 // serve static files
 app.get('/*', function(req, res) {
-  var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(__dirname, './public', file));
+    var file = req.params[0] || '/views/index.html';
+    res.sendFile(path.join(__dirname, './public', file));
 });
 
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function() {
-  console.log('server is running on port ', app.get('port'));
+    console.log('server is running on port ', app.get('port'));
 });
--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,67 +1,67 @@
 $(document).ready(function() {
 
-  loadCats();
+    loadCats();
 
-  function loadCats() {
-    $.ajax({
-      type: 'GET',
-      url: '/felines',
-      success: function(response) {
-        $("#cat-list").empty();
-        response.forEach(function(cat) {
-          appendDom(cat);
+    function loadCats() {
+        $.ajax({
+            type: 'GET',
+            url: '/cats',
+            success: function(response) {
+                $("#cat-list").empty();
+                response.forEach(function(cat) {
+                    appendDom(cat);
+                });
+            }
+        })
+    }
+
+    function appendDom(cat) {
+        $("#cat-list").append('<div class="cat"></div>');
+        $el = $("#cat-list").children().last();
+        $el.append('<h2>' + cat.name + '</h2>');
+        $el.append('<p>' + cat.trait + '</p>');
+    }
+
+
+    $("#cat-form").on("submit", function(event) {
+        event.preventDefault();
+        var cat = {};
+
+        $.each($("#cat-form").serializeArray(), function(i, field) {
+            cat[field.name] = field.value;
         });
-      }
-    })
-  }
 
-  function appendDom(cat) {
-    $("#cat-list").append('<div class="cat"></div>');
-    $el = $("#cat-list").children().last();
-    $el.append('<h2>' + cat.name + '</h2>');
-    $el.append('<p>' + cat.trait + '</p>');
-  }
+        console.log('cat to send: ', cat);
 
+        $.ajax({
+            type: 'POST',
+            url: '/cats',
+            data: cat,
+            success: function(data) {
+                console.log('post request successful!');
+                loadCats();
+            },
+            error: function() {
+                console.log('post failed');
+                alert("hey, it didn't work!");
+            }
 
-  $("#cat-form").on("submit", function(event) {
-    event.preventDefault();
-    var cat = {};
+        });
 
-    $.each($("#cat-form").serializeArray(), function(i, field) {
-      cat[field.name] = field.value;
-    });
+        /**
+        Which is the same as:
+        $(this).serializeArray().forEach(function(field, index) {
+          cat[field.name] = field.value;
+        });
 
-    console.log('cat to send: ', cat);
-
-    $.ajax({
-      type: 'POST',
-      url: '/kitties',
-      data: cat,
-      success: function(data) {
-        console.log('post request successful!');
-        loadCats();
-      },
-      error: function() {
-        console.log('post failed');
-        alert("hey, it didn't work!");
-      }
+        This a method chain of:
+          var fields = $(this).serializeArray();
+          fields.forEach(function(field, index) {
+            cat[field.name] = field.value;
+          });
+        **/
 
     });
-
-    /**
-    Which is the same as:
-    $(this).serializeArray().forEach(function(field, index) {
-      cat[field.name] = field.value;
-    });
-
-    This a method chain of:
-      var fields = $(this).serializeArray();
-      fields.forEach(function(field, index) {
-        cat[field.name] = field.value;
-      });
-    **/
-
-  });
 
 
 });
